910d1b2c0b00d8e258d3508855d32fa05cc469d4
Airyi <2739327400@qq.com>
2019-06-27 17:42:02 +0800

    6.27

--- app/src/main/java/net/lzzy/algorithm/algorlib/BubbleSort.java
+++ app/src/main/java/net/lzzy/algorithm/algorlib/BubbleSort.java
@@ -4,7 +4,27 @@ package net.lzzy.algorithm.algorlib;
  * Created by lzzy_gxy on 2019/6/22.
  * Description:
  */
-public  abstract  class BubbleSort {
+//
+public  abstract  class BubbleSort <T extends Comparable<? super T>> extends BaseSort<T> {
+
+    BubbleSort(T[] items) {
+        super(items);
+    }
+
+    @Override
+    public void sort() {
+        long start=System.currentTimeMillis();
+        for (int i=0;i<items.length-1;i++){
+            int minpos=i;
+            for (int j=i+1;j<items.length;j++){
+                if (bigger(items[minpos],items[j+1])){
+                    swap(minpos,i);
+                }
+            }
+        }
+        duration=System.currentTimeMillis()-start;
+    }
+
 
 
 }
