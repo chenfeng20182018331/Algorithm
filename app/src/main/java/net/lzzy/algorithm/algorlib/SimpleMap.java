a5c9c5c52483525d2720d191e2732a6844f9fdf6
Airyi <2739327400@qq.com>
2019-07-08 16:09:31 +0800

    7.8

--- app/src/main/java/net/lzzy/algorithm/algorlib/SimpleMap.java
+++ app/src/main/java/net/lzzy/algorithm/algorlib/SimpleMap.java
@@ -1,8 +1,56 @@
 package net.lzzy.algorithm.algorlib;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by lzzy_gxy on 2019/6/27.
  * Description:
  */
 public class SimpleMap {
+    int vertexCount;
+    List<Edge> edges=new ArrayList<>();
+List<Integer> visited;
+    public SimpleMap(int v){
+        vertexCount=v;
+    }
+
+    
+    
+    
+    public void addEdge(int source,int targe,double distance){
+        edges.add(new Edge(source, targe,distance));
+    }
+    public void addTwoEdge(int v1,int v2,double distance){
+       addEdge(v1,v2,distance);
+        addEdge(v1,v2,distance);
+    }
+    
+    
+
+    public void iteratRangeFirst(int v){
+        if (!visited.contains(v)){
+            visited.add(v);
+        }
+        if (visited.size()==vertexCount){
+            return;
+        }
+    }
+    public String iterateDepthFirst(){
+        return "012354";
+    }
+}
+class Edge{
+    private int source;
+    private int target;
+    private double distance;
+
+    Edge(int source,int target,double distance){
+        this.source=source;
+        this.target=target;
+        this.distance=distance;
+    }
+    public void add(){
+
+    }
 }
